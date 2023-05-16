d2d88c3f84e864dcb3d640c8f021e79433c02135
lalu arif trasna ashari <93263857+LALUARIFTA@users.noreply.github.com>
2023-05-16 12:45:32 +0800

    Add files via upload

---
+++ hasil.php
@@ -0,0 +1,10 @@
+<?php 
+// get
+// $nama = $_GET["username"];
+// $pass = $_GET["password"];
+
+// post
+$nama = $_POST["username"];
+$pass = $_POST["password"];
+echo "nama saya " .$nama . " password saya adalah " .$pass;
+?>
---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <H1>LATIHAN FORM PHP</H1>
+    <!-- <form method="get" action="hasil.php">
+        <label>
+            username
+            <input type="text" name="username">
+        </label><br>
+
+        <label>
+            password
+            <input type="password" name="password">
+        </label><br>
+        <button type="submit">kirim</button>
+    </form> -->
+    <form method="post" action="hasil.php">
+        <label>
+            username
+            <input type="text" name="username">
+        </label><br>
+
+        <label>
+            password
+            <input type="password" name="password">
+        </label><br>
+        <button type="submit">kirim</button>
+    </form>
+</body>
+
+</html>
